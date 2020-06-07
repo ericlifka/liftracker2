8821955753d8fac8394bc0ccd937fe14606250f2
Eric Lifka <eric.lifka@lifeomic.com>
2020-06-07 18:39:34 -0400

    setup react

--- src/app.tsx
+++ src/app.tsx
@@ -1,1 +1,8 @@
-alert("hello world");
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+function App() {
+  return <div>Hello World</div>;
+}
