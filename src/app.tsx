ba04e5c12ea88a3d95841208e7d96b7912b94094
Eric Lifka <eric.lifka@lifeomic.com>
2020-06-12 16:33:47 -0400

    base app to build from, save counter as example to refer back to

--- src/app.tsx
+++ src/app.tsx
@@ -1,94 +1,51 @@
-import React, { useState, FunctionComponent } from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom"
-import { createStore } from "redux"
+import { createStore, applyMiddleware } from "redux"
+import thunkMiddleware from 'redux-thunk'
+import { createLogger as createLoggerMiddleware } from 'redux-logger'
 
 
 // -- Model
 
 type Model =
-  { counter: number
+  {
   }
 
 const init: () => Model =
 () => (
-  { counter: 0
+  {
   })
 
 
 // -- Update
 
 enum Actions
-  { increment
-  , decrement
-  , reset
+  {
   }
-
-type IncrementAction =
-  { type: Actions.increment
-  , quantity: number
-  }
-
-const increment: (quantity: number) => IncrementAction =
-(quantity) => (
-  { type: Actions.increment
-  , quantity
-  })
-
-type DecrementAction =
-  { type: Actions.decrement
-  , quantity: number
-  }
-
-const decrement: (quantity: number) => DecrementAction =
-(quantity) => (
-  { type: Actions.decrement
-  , quantity
-  })
-
-type ResetAction =
-  { type: Actions.reset
-  }
-
-const reset: () => ResetAction =
-() => (
-  { type: Actions.reset
-  })
 
 type Msg
-  = IncrementAction
-  | DecrementAction
-  | ResetAction
+  = { type: Actions }
 
-const update: (model: Model, msg: Msg) => Model =
+const update: (m: Model, ms: Msg) => Model =
 (model, msg) => {
   switch (msg.type) {
-    case Actions.increment: return (
-      { ...model
-      , counter: model.counter + msg.quantity
-      })
-
-    case Actions.decrement: return (
-      { ...model
-      , counter: model.counter - msg.quantity
-      })
-
-    case Actions.reset: return (
-      { ... model
-      , counter: 0
-      })
-
     default: return model
   }
 }
 
-const { dispatch, getState, subscribe } = createStore(update, init())
+
+// -- Store
+
+const middleware = applyMiddleware(thunkMiddleware, createLoggerMiddleware())
+const store = createStore(update, init(), middleware)
+const { dispatch, getState, subscribe } = store
 
 
 // -- View
 
 type Html = JSX.Element
 
-const View: FunctionComponent =
+const View: () => Html =
 () => {
   const [model, setModel] = useState<Model>(getState())
   subscribe(() => setModel(getState()))
@@ -96,26 +53,10 @@ const View: FunctionComponent =
   return <App model={model} />
 }
 
-type AppProps =
-  { model: Model
-  }
-
-const App: FunctionComponent<AppProps> =
+const App: (props: { model: Model }) => Html =
 ({ model }) =>
-  <div>
-    <div style={{ display: "flex" }}>
-      {button("-1", decrement(1))}
-      <div>{model.counter}</div>
-      {button("+1", increment(1))}
-    </div>
-    <div style={{ display: "flex" }}>
-      {button("clear", reset())}
-    </div>
-  </div>
+  <div></div>
 
-const button: (text: string, msg: Msg) => Html =
-(text, msg) =>
-  <button onClick={() => dispatch(msg)}>{text}</button>
 
 // -- Main
 
